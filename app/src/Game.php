90089867abaffff6a1561d629165d6241edc5ee9
Thomas Le Duc <dev@tleduc.de>
2020-08-03 10:44:43 +0200

    Third try

--- app/src/Game.php
+++ app/src/Game.php
@@ -6,4 +6,37 @@ namespace App;
 
 final class Game
 {
+    /** @var array<int, int> */
+    private array $rolls = [];
+
+    public function roll(int $pins): void
+    {
+        $this->rolls[] = $pins;
+    }
+
+    public function score(): int
+    {
+        $score = 0;
+        foreach (array_keys($this->rolls) as $rollCount)
+            $score += $this->calculateScoreForRoll($rollCount);
+
+        return $score;
+    }
+
+    private function calculateScoreForRoll(int $rollCount): int
+    {
+        $rollScore = $this->rolls[$rollCount];
+        if ($rollCount % 2 !== 0)
+            $rollScore += $this->calulateSpareBonus($rollCount);
+
+        return $rollScore;
+    }
+
+    private function calulateSpareBonus(int $rollCount): int
+    {
+        if ($this->rolls[$rollCount] + $this->rolls[$rollCount - 1] === 10)
+            return $this->rolls[$rollCount + 1];
+
+        return 0;
+    }
 }

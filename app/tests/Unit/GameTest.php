922315ad4659f92b04ce5f2469dc01e68b3980c8
Thomas Le Duc <dev@tleduc.de>
2020-09-02 07:21:00 +0200

    Change from CodeSniffer to PHP-CS-Fixer

--- app/tests/Unit/GameTest.php
+++ app/tests/Unit/GameTest.php
@@ -7,93 +7,14 @@ namespace App\Tests;
 use App\Game;
 use PHPUnit\Framework\TestCase;
 
-/** @covers Game */
+/** @covers \Game */
 final class GameTest extends TestCase
 {
     /** @test */
     public function canCreateABowlingGame(): void
     {
-        $bowling = $this->createBowlingGame();
+        $bowling = new Game();
 
         self::assertInstanceOf(Game::class, $bowling);
-    }
-
-    /**
-     * @test
-     * @testWith
-     * [0, 0]
-     * [1, 1]
-     * [2, 2]
-     */
-    public function rollXPins(int $pins, int $expectedScore): void
-    {
-        $bowling = $this->createBowlingGame();
-
-        $bowling->roll($pins);
-
-        self::assertSame($expectedScore, $bowling->score());
-    }
-
-    /** @test */
-    public function initialGameScoreIs0(): void
-    {
-        $bowling = $this->createBowlingGame();
-
-        $score = $bowling->score();
-
-        self::assertSame(0, $score);
-    }
-
-    /**
-     * @test
-     * @testWith
-     * [0, 0]
-     * [1, 20]
-     * [2, 40]
-     */
-    public function rollAllX(int $pins, int $expectedScore): void
-    {
-        $bowling = $this->createBowlingGame();
-
-        $this->roll20TimesXPins($bowling, $pins);
-
-        self::assertSame($expectedScore, $bowling->score());
-    }
-
-    /** @test */
-    public function bonusForSpare(): void
-    {
-        $bowling = $this->createBowlingGame();
-
-        $bowling->roll(5);
-        $bowling->roll(5);
-        $bowling->roll(1);
-
-        self::assertSame(12, $bowling->score());
-    }
-
-    /**
-     * @test
-     * @requires PHP = 6
-     */
-    public function rollAllSpares(): void
-    {
-        $bowling = $this->createBowlingGame();
-
-        $this->roll20TimesXPins($bowling, 5);
-        $bowling->roll(5);
-
-        self::assertSame(150, $bowling->score());
-    }
-
-    private function createBowlingGame(): Game
-    {
-        return new Game();
-    }
-
-    private function roll20TimesXPins(Game $bowling, int $pins): void
-    {
-        for ($i = 0; $i < 20; $i++)
-            $bowling->roll($pins);
     }
 }

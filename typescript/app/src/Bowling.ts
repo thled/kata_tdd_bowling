59e577d526c51c4db997b3db3e7a1cdd8dba643a
Thomas Le Duc <dev@tleduc.de>
2020-09-18 10:08:35 +0200

    Successful completion in TypeScript

--- typescript/app/src/Bowling.ts
+++ typescript/app/src/Bowling.ts
@@ -1,11 +1,53 @@
 export class Bowling {
-    private score: number = 0
+    private rolls: Array<number> = []
 
-    totalScore(): number {
-        return this.score
+    public roll(pins: number): void {
+        this.rolls.push(pins)
     }
 
-    roll(pins: number): void {
-        this.score += pins
+    public score(): number {
+        let score: number = 0
+
+        for (
+            let frame: number = 0, firstInFrame: number = 0;
+            frame < 10;
+            frame++, firstInFrame += 2
+        ) {
+            score += this.frameScore(firstInFrame)
+
+            if (this.isSpare(firstInFrame))
+                score += this.spareBonus(firstInFrame)
+
+            if (this.isStrike(firstInFrame)) {
+                score += this.strikeBonus(firstInFrame)
+
+                firstInFrame--
+            }
+        }
+
+        return score
+    }
+
+    private strikeBonus(firstInFrame: number): number {
+        return this.rolls[firstInFrame + 1] + this.rolls[firstInFrame + 2]
+    }
+
+    private isStrike(firstInFrame: number): boolean {
+        return this.rolls[firstInFrame] === 10
+    }
+
+    private spareBonus(firstInFrame: number): number {
+        return this.rolls[firstInFrame + 2]
+    }
+
+    private isSpare(roll: number): boolean {
+        return this.rolls[roll] + this.rolls[roll + 1] === 10
+    }
+
+    private frameScore(firstInFrame: number): number {
+        if (this.isStrike(firstInFrame)) return this.rolls[firstInFrame]
+
+        return this.rolls[firstInFrame] + this.rolls[firstInFrame + 1]
     }
 }
+
--- typescript/app/tests/Game.test.ts
+++ typescript/app/tests/Game.test.ts
@@ -23,16 +23,81 @@ describe('Bowling', () => {
     it('gutter game', () => {
         rollMany(20, 0)
 
-        expect(bowling.totalScore()).toBe(0)
+        expect(bowling.score()).toBe(0)
     })
 
     it('all ones', () => {
         rollMany(20, 1)
 
-        expect(bowling.totalScore()).toBe(20)
+        expect(bowling.score()).toBe(20)
+    })
+
+    it('spare', () => {
+        rollSpare()
+        rollMany(18, 0)
+
+        expect(bowling.score()).toBe(10)
+    })
+
+    it('spare with bonus', () => {
+        rollSpare()
+        bowling.roll(1)
+        rollMany(17, 0)
+
+        expect(bowling.score()).toBe(10 + 1 + 1)
+    })
+
+    it('no spare between frames', () => {
+        bowling.roll(0)
+        rollSpare()
+        bowling.roll(1)
+        rollMany(16, 0)
+
+        expect(bowling.score()).toBe(10 + 1)
+    })
+
+    it('strike', () => {
+        rollStrike()
+        rollMany(18, 0)
+
+        expect(bowling.score()).toBe(10)
+    })
+
+    it('strike with bonus', () => {
+        rollStrike()
+        bowling.roll(1)
+        bowling.roll(1)
+        rollMany(16, 0)
+
+        expect(bowling.score()).toBe(10 + 2 + 1 + 1)
+    })
+
+    it('no strike if second roll in frame', () => {
+        bowling.roll(0)
+        rollStrike()
+        bowling.roll(1)
+        bowling.roll(1)
+        rollMany(16, 0)
+
+        expect(bowling.score()).toBe(10 + 1 + 1 + 1)
+    })
+
+    it('perfect game', () => {
+        rollMany(12, 10)
+
+        expect(bowling.score()).toBe(300)
     })
 
     function rollMany(rolls: number, pins: number): void {
         for (let roll = 0; roll < rolls; roll++) bowling.roll(pins)
+    }
+
+    function rollSpare(): void {
+        bowling.roll(5)
+        bowling.roll(5)
+    }
+
+    function rollStrike(): void {
+        bowling.roll(10)
     }
 })

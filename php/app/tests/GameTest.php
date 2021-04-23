76b26df87635e09274831f6edae8e4a5bd144c31
Thomas Le Duc <dev@tleduc.de>
2021-04-23 09:57:58 +0200

    Add shorthands plugin for Pest

--- php/app/tests/GameTest.php
+++ php/app/tests/GameTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
+use function Thled\Shorthands\fit;
+use function Thled\Shorthands\xit;
+
 it('works', function () {
     expect(true)->toBeTrue();
 });
